4a7f45ea91f33f04fb03549bc6ce4d60d5c98958
GibreelAbdullah <gibreel.khan@gmail.com>
2020-06-02 21:44:20 +0530

    Little code refactoring + Enabling on r/izlanimemes

--- BrozzerAbdullah.py
+++ BrozzerAbdullah.py
@@ -5,6 +5,7 @@ import time
 import re
 import logging
 import sys
+from constants import constants
 from random import randint
 from linereader import copen
 
@@ -20,66 +21,60 @@ def bot_login():
 
 
 def run_bot(r):
-    takbirList = ['takbir','takbeer','tekbir','تكبير']
-    taqiyaList = ['taqiyya','taqqiyya','taqiya','taqqiya']
-    jazakallahList = ['jazakallah','jazakillah','jazakumullah']
-        
-    footer = "\n^[r/MuslimDevNetwork](https://www.reddit.com/r/MuslimDevNetwork/) ^|| ^[r/Sahaba](https://www.reddit.com/r/Sahaba/) ^|| ^[r/MuslimLounge](https://www.reddit.com/r/MuslimLounge/)"
-
-    comment_stream = r.subreddit('Izlam').stream.comments(pause_after=-1)
-    submission_stream = r.subreddit('Izlam').stream.submissions(pause_after=-1)
+    comment_stream = r.subreddit(constants.subreddits).stream.comments(pause_after=-1,skip_existing=True)
+    submission_stream = r.subreddit(constants.subreddits).stream.submissions(pause_after=-1,skip_existing=True)
     while True:
         for comment in comment_stream:
             if (comment is None):
                 break
             comment_text = comment.body.lower()
             reply_comment = ""
-            if("good bot" in comment_text and comment.parent().author == r.user.me()):
-                    # logging.info ("Found good bot in " + comment.id)
-                    reply_comment = "Good Human. " + get_random_dua() + "\n\n"
-            if("bad bot" in comment_text and comment.parent().author == r.user.me()):
-                # logging.info ("Found bad bot in " + comment.id)
+            if("good bot" in comment_text and comment.parent().author == r.user.me() and comment.subreddit in ['Izlam','izlanimemes']):
+                print ("Found good bot in https://www.reddit.com" + comment.permalink)
+                reply_comment = "Good Human. " + get_random_dua() + "\n\n"
+            if("bad bot" in comment_text and comment.parent().author == r.user.me() and comment.subreddit in ['Izlam','izlanimemes']):
+                print ("Found bad bot in https://www.reddit.com" + comment.permalink)
                 reply_comment = reply_comment + "[Behave yourself!](https://i.ytimg.com/vi/oL15on_OyBA/hqdefault.jpg)\n\n"
-            if any(takbir in comment_text for takbir in takbirList):
-                # logging.info ("Found Takbir in " + comment.id)
+            if any(takbir in comment_text for takbir in constants.takbirList) and comment.subreddit in ['Izlam','izlanimemes']:
+                print ("Found Takbir in https://www.reddit.com" + comment.permalink)
                 reply_comment = reply_comment + "#الله اكبر  ALLAHU AKBAR!!!!\n\n"
-            if any(taqiya in comment_text for taqiya in taqiyaList) and not comment.author == r.user.me() :
-                # logging.info ("Found Taqiya in " + comment.id)
+            if any(taqiya in comment_text for taqiya in constants.taqiyaList) and not comment.author == r.user.me() and comment.subreddit in ['Izlam','izlanimemes']:
+                print ("Found Taqiya in https://www.reddit.com" + comment.permalink)
                 reply_comment = reply_comment + "This brozzer/sizter is using taqqiya, 100% true taqqiya master\n\n"
-            if ("staff gorilla" in comment_text):
-                # logging.info ("Found staff gorilla in " +comment.id)
+            if ("staff gorilla" in comment_text and comment.subreddit in ['Izlam','izlanimemes']):
+                print ("Found staff gorilla in https://www.reddit.com" + comment.permalink)
                 reply_comment = reply_comment + "[You called me?](https://imgur.com/T60vscc)\n\n"
-            if any(jazakallah in comment_text for jazakallah in jazakallahList) and comment.parent().author == r.user.me():
-                # logging.info ("Found jazakallah in " + comment.id)
+            if any(jazakallah in comment_text for jazakallah in constants.jazakallahList) and comment.parent().author == r.user.me() and comment.subreddit in ['Izlam','izlanimemes']:
+                print ("Found jazakallah in https://www.reddit.com" + comment.permalink)
                 reply_comment = reply_comment + "وأنتم فجزاكم الله خيرا Wa antum, fa jazakumullahu khairan\n\n"
             if reply_comment!="":
                 logging.info ("Replying to comment : " + comment.body)
-                # logging.info(reply_comment)
-                reply_comment = reply_comment + footer
+                print(reply_comment)
+                reply_comment = reply_comment + constants.footer
                 comment.reply(reply_comment)
         for submission in submission_stream:
             if(submission is None):
                 break
             submission_text = submission.title.lower() + "------\n" + submission.selftext.lower()
             reply_comment = ""
-            if any(taqiya in submission_text for taqiya in taqiyaList):
-                # logging.info("Taqiya in Post : " + submission.id)
+            if any(taqiya in submission_text for taqiya in constants.taqiyaList) and submission.subreddit in ['Izlam','izlanimemes']:
+                print("Taqiya in Post : " + submission.permalink)
                 reply_comment = "Sniff, sniff... I smell Taqiya\n\n"
-            if any(takbir in submission_text for takbir in takbirList):
-                # logging.info ("Found Takbir in " + submission.id)
+            if any(takbir in submission_text for takbir in constants.takbirList) and submission.subreddit in ['Izlam','izlanimemes']:
+                print ("Found Takbir in " + submission.permalink)
                 reply_comment = reply_comment + "#الله اكبر  ALLAHU AKBAR!!!!\n\n"
-            if ("staff gorilla" in submission_text):
-                # logging.info ("Found staff gorilla in " +submission.id)
+            if ("staff gorilla" in submission_text) and submission.subreddit in ['Izlam','izlanimemes']:
+                print ("Found staff gorilla in " +submission.permalink)
                 reply_comment = reply_comment + "[You called me?](https://imgur.com/T60vscc)\n\n"
             if reply_comment!="":
-                logging.info ("Replying to comment : " + submission.id)
-                # logging.info(reply_comment)
-                reply_comment = reply_comment + footer
+                logging.info ("Replying to comment : " + submission.permalink)
+                print(reply_comment)
+                reply_comment = reply_comment + constants.footer
                 submission.reply(reply_comment)
 
 
 def get_random_dua():
-    openfile = copen("dua.txt")
+    openfile = copen("./constants/dua.txt")
     lines = openfile.count('\n') + 1
     dua = openfile.getline(randint(1,lines))
     return dua
---
+++ constants/constants.py
@@ -0,0 +1,9 @@
+#Add subreddits subredditList with a + sign in a single string literal
+#Eg = subredditList = 'Izlam+islam+MuslimLounge' 
+subreddits = 'Izlam+izlanimemes'
+
+takbirList = ['takbir','takbeer','tekbir','تكبير']
+taqiyaList = ['taqiyya','taqqiyya','taqiya','taqqiya']
+jazakallahList = ['jazakallah','jazakillah','jazakumullah']
+
+footer = "\n^[r/MuslimDevNetwork](https://www.reddit.com/r/MuslimDevNetwork/) ^|| ^[r/Sahaba](https://www.reddit.com/r/Sahaba/) ^|| ^[r/MuslimLounge](https://www.reddit.com/r/MuslimLounge/)"
